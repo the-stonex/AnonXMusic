a682e55b4ea0a991302f8fe5c6ae1889adee3745
ᴛʜᴇ ᴄʜᴧᴍᴘᴜ <112340896+ChampuXD@users.noreply.github.com>
2024-04-21 18:33:41 +0530

    Update start.py
    Signed-off-by: ᴛʜᴇ ᴄʜᴧᴍᴘᴜ <112340896+ChampuXD@users.noreply.github.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -32,8 +32,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_photo(        
-               
+            return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
@@ -86,9 +85,6 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            client.effective_message.reply_sticker(
-            "CAACAgEAAxkBAAID3GYknDkwl4foDoKtlad7wNvJEqqNAAL2AwACKhGpRE51sjSpEHMyNAQ"
-            ),
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
@@ -106,9 +102,6 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        client.effective_message.reply_sticker(
-        "CAACAgEAAxkBAAID3GYknDkwl4foDoKtlad7wNvJEqqNAAL2AwACKhGpRE51sjSpEHMyNAQ"
-        ),
         photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
@@ -144,9 +137,6 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(        
-                    client.effective_message.reply_sticker(
-                    "CAACAgEAAxkBAAID3GYknDkwl4foDoKtlad7wNvJEqqNAAL2AwACKhGpRE51sjSpEHMyNAQ"
-                    ),
                     photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
